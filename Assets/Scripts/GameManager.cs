b8205fa1534e43ee5873791ad961f07e3441245a
Jawad Chodhri <152736441+jawadchodhri@users.noreply.github.com>
2026-04-14 13:24:18 +0500

    Main Menu Built
    Main Menu Built at 8:48

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -17,7 +17,9 @@ public class GameManager : MonoBehaviour
     private State state;
     private float waitingToStartTimer = 1f;
     private float countdownToStartTimer = 3f;
-    private float gamePlayingTimer = 10f;
+    private float gamePlayingTimer;
+    private float gamePlayingTimerMax = 10f;
+
 
     private void Awake()
     {
@@ -44,6 +46,7 @@ public class GameManager : MonoBehaviour
                 if (countdownToStartTimer < 0f)
                 {
                     state = State.GamePlaying;
+                    gamePlayingTimer = gamePlayingTimerMax;
                     OnStateChanged?.Invoke(this, EventArgs.Empty);
                 }
                 break;
@@ -76,5 +79,15 @@ public class GameManager : MonoBehaviour
     public float GetCountdownToStartTimer()
     {
         return countdownToStartTimer;
+    }
+
+    public bool IsGameOver()
+    {
+        return state == State.GameOver;
+    }
+
+    public float GetGamePlayingTimerNormalized()
+    {
+        return 1 - (gamePlayingTimer / gamePlayingTimerMax);
     }
 }
---
+++ Assets/Scripts/GameOverUI.cs
@@ -0,0 +1,39 @@
+using TMPro;
+using UnityEngine;
+
+public class GameOverUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI recipesDeliveredText;
+
+    private void Start()
+    {
+        GameManager.Instance.OnStateChanged += GameManager_OnStateChanged;
+
+        Hide();
+    }
+
+    private void GameManager_OnStateChanged(object sender, System.EventArgs e)
+    {
+        if (GameManager.Instance.IsGameOver())
+        {
+            Show();
+
+            recipesDeliveredText.text = DeliveryManager.Instance.GetSccessfulRecipesAmount().ToString();
+        }
+        else
+        {
+            Hide();
+        }
+    }
+
+
+    private void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    private void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
